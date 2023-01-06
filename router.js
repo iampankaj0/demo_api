f20e67343c98304490a8bfa5437d30fc8b7e73ee
Pankaj Yadav <91059277+iampankaj0@users.noreply.github.com>
2023-01-06 13:08:09 +0530

    initial

--- router.js
+++ router.js
@@ -4,11 +4,12 @@ const router = express.Router();
 
 router.get("/user", (req, res, next) => {
   res.json({
-    user: {
-      name: "Pankaj Yadav",
-      village: "Mandola",
-      pin: "123103",
-    },
+    user: [
+      { name: "Pankaj Yadav", village: "Mandola", pin: "123103" },
+      { name: "Pankaj Yadav", village: "Mandola", pin: "123103" },
+      { name: "Pankaj Yadav", village: "Mandola", pin: "123103" },
+      { name: "Pankaj Yadav", village: "Mandola", pin: "123103" },
+    ],
     success: true,
   });
 });
